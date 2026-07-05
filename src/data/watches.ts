4a5217e6b7e8a4b8c66919fa03f718f55a1e6eff
Nadia Escobbb <devcodernadia@gmail.com>
2026-07-05 02:47:03 -0300

    fix: hardcode watches image cloudinary URLs

--- src/data/watches.ts
+++ src/data/watches.ts
@@ -13,21 +13,21 @@ export const watches: Watch[] = [
     id: 'edifice',
     title: 'Edifice',
     category: 'Línea de acero',
-    image: getImageUrl('/edefice1_gzc3qa.avif'),
+    image: 'https://res.cloudinary.com/dsdb4fibv/image/upload/v1782508797/edefice1_gzc3qa.avif',
     waMessage: 'Hola Cuore, quería consultar por relojes Casio Edifice'
   },
   {
     id: 'vintage',
     title: 'Vintage',
     category: 'Clásicos retro',
-    image: getImageUrl('/clasico_b9c6rp.avif'),
+    image: 'https://res.cloudinary.com/dsdb4fibv/image/upload/v1782508794/clasico_b9c6rp.avif',
     waMessage: 'Hola Cuore, quería consultar por relojes retro/vintage'
   },
   {
     id: 'gshock',
     title: 'G-Shock',
     category: 'Alta resistencia',
-    image: getImageUrl('/gshock_twz9qg.avif'),
+    image: 'https://res.cloudinary.com/dsdb4fibv/image/upload/v1782508796/gshock_twz9qg.avif',
     waMessage: 'Hola Cuore, quería consultar por modelos G-Shock'
   }
 ];
